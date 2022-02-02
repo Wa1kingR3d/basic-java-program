5969f8ec37a764ca1b4587355586c0ad00d8e1f9
Wa1kingR3d <97804810+Wa1kingR3d@users.noreply.github.com>
2022-02-02 15:49:27 -0800

    Add files via upload
    read the instructions in the file

---
+++ Template.java
@@ -0,0 +1,13 @@
+/*
+  Use this code snipit to get a fresh file that you can start
+  right off the bat.
+
+  DELETE THIS COMMENT STRING AFTER DUPLICATING
+
+  - Template.java > right click > duplicate
+*/
+public class Template { //rename "Template"
+  public static void main(String[] args) {
+
+  }
+}
